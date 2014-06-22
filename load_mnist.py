c3f6db376bad1788403d71bd51d8424611599513
nyghtowl <warrick.melanie@gmail.com>
2014-06-22 09:10:06 -0700

    Adding code to show the image. Added code to notebook and script. Not currently working which suspect is data format.

--- load_mnist.py
+++ load_mnist.py
@@ -3,8 +3,22 @@ Code to load the sample MNIST file
 '''
 import cPickle, gzip
 import numpy as np
+import Image
 
 def load_file(filename):
     with gzip.open(filename) as f:
         train_set, valid_set, test_set = cPickle.load(f)
     return train_set, valid_set, test_set
+
+# Not working - possibly need to reformat data into numpy...
+def show_img(x):
+    im = Image.new('L', (28, 28))
+    im.putdata(x, scale=256)
+    im.show()
+
+def main():
+    train_set, valid_set, test_set = load_file('../mnist.pkl.gz')
+    show_img(train_set)
+
+if __name__ == "__main__":
+    main()
