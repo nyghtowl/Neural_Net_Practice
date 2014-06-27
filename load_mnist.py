bc92a93499dc6815de3d80ca3539426a3bcddfb2
nyghtowl <warrick.melanie@gmail.com>
2014-06-27 08:27:39 -0700

    Found a hacky solution to swap out the y values. Need to verify if that resolve dbn issue. Also, need to check on why numpy filter won't swap num for list value.

--- load_mnist.py
+++ load_mnist.py
@@ -26,34 +26,38 @@ def pull_sample(data, size, target):
     y = data[1][data[1]==target][0:size]
     return x, y
 
-# def binarize(num):
-#     # need to convert y labels
-#     if num == 0:
-#         return [1,0]
-#     else:
-#         return [0,1]
+def binarize(num):
+    if num == 0:
+        return [1,0]
+    else:
+        return [0,1]
 
-def build_sample(data):
+def build_sample(data, size):
     x_results, y_results = [], []
     for value in xrange(0,2):
-        x, y = pull_sample(data, 10, value)
+        x, y = pull_sample(data, size, value)
+        print "y", y
         x_results.append(x)
-        y_results.append(y)
+        for num in y:
+            y_results.append(binarize(num)) # find better switch
+    print "y_results", y_results
 
     x_sample = np.vstack(x_results)
-    y_sample = np.concatenate(y_results)
+    y_sample = np.vstack(y_results)
 
+    print "y_sample", y_sample
     result = zip(x_sample, y_sample)
     random.shuffle(result)
     return zip(*result)
 
 
 def main():
+    size = 10
     train_set, valid_set, test_set = load_file('../mnist.pkl.gz')
     #show_img(train_set[0][100]) # see example of image
     #print train_set[1][100] # confirm label associated
-    pics, labels = build_sample(train_set)
-    return DBN.DBN(input=pics, label=labels, n_ins=784, hidden_layer_sizes=[500, 250, 100], n_outs=10, numpy_rng=None)
+    pics, labels = build_sample(train_set, size)
+    #return DBN.DBN(input=pics, label=labels, n_ins=784, hidden_layer_sizes=[500, 250, 100], n_outs=10, numpy_rng=None)
 
 
 
